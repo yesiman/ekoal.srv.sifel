2c972fbaebaed52321adfe9d0300786817e062af
fabio <fab.grenouillet@gmail.com>
2016-11-19 15:49:23 +0400

    init

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 exports.getUser = function (req, res) {
     //var token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['x-access-token'];
-    res.send("Hello" + req.params.idp);
+    res.send("Hello" + req.params.id);
 };
 exports.getUsers = function (req, res) {
     //var token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['x-access-token'];
