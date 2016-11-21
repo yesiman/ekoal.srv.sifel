88ca750209ec93e99ed47eab1d9b77c8a2e16c42
fabio <fab.grenouillet@gmail.com>
2016-11-21 14:25:11 +0400

    test add prod

--- routes/products.js
+++ routes/products.js
@@ -14,7 +14,7 @@ exports.delete = function (req, res) {
 exports.add = function (req, res) {
     //var token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['x-access-token'];
     db.collection('products', function (err, collection) {
-        collection.insert( req.params.product , function (err, saved) {
+        collection.insert( req.body.product , function (err, saved) {
             if (err || !saved) {
                 res.send(false)
             }
