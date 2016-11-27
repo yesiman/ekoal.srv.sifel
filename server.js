a8e531e15e3e3aa7f3af1512cc73eed132311e87
fabio <fab.grenouillet@gmail.com>
2016-11-27 15:36:58 +0400

    tokenize tests

--- server.js
+++ server.js
@@ -51,8 +51,23 @@ app.post('/users/login', cors(), bodyParser.json(), users.login);
 //TOKEN VALIDATION
 app.use(function(req, res, next) {
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
-    console.log(token);
-    next();
+    if (token)
+    {
+        jwt.verify(token, process.env.JWT, function(err, decoded) {      
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });    
+            } else {
+                req.decoded = decoded;    
+                next();
+            }
+        });
+    }
+    else {
+        return res.status(403).send({ 
+            success: false, 
+            message: 'No token provided.' 
+        });
+    }
 });
 //END TOKEN VALIDATION
 //USERS
