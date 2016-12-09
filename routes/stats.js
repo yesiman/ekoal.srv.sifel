b1c124ec223dc33ba673000735ef2d322a0ee0c7
fabio <fab.grenouillet@gmail.com>
2016-12-09 13:05:12 +0400

    'stats'

--- routes/stats.js
+++ routes/stats.js
@@ -1,13 +1,14 @@
 exports.prevsByDay = function (req, res) {
     db.collection('planifs_lines', function (err, collection) {
         collection.aggregate(
+            {},
             { $group : {
                 _id: {
                     year : { $year : "$datePlant" },        
                     month : { $month : "$datePlant" },        
                     day : { $dayOfMonth : "$datePlant" },
                 },
-                count: { $sum: 1 }
+                count: { $sum: $qte }
             }}, // you can only project fields from 'group'
             function(err, summary) {
                 console.log(summary);
