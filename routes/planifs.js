f6bb6e396b90daffca19e3a7b85974fdc7cab00b
fabio <fab.grenouillet@gmail.com>
2016-12-09 09:41:07 +0400

    add get by type method

--- routes/planifs.js
+++ routes/planifs.js
@@ -1,15 +1,18 @@
 exports.add = function (req, res) {
     var pid = req.params.id;
     req.body.planif.dateModif = new Date();
+    var lines = req.body.planif.lines;
+    delete req.body.planif.lines;
     db.collection('planifs', function (err, collection) {
         if (pid == "-1")
         {
             collection.insert( req.body.planif , function (err, saved) {
                 if (err || !saved) {
-                    res.send(false)
+                    pid = saved.insertedIds[0];
+                    //res.send(false)
                 }
                 else {
-                    res.send(true);
+                    //res.send(true);
                 }
             });
         }
@@ -18,7 +21,15 @@ exports.add = function (req, res) {
             collection.update(
                 { _id: new require('mongodb').ObjectID(pid) },
                 req.body.planif);
-                res.send(true);
-        }      
+                //res.send(true);
+        }
     });
+    db.collection('planifs_lines', function (err, collection) {
+        for (var i = 0; i < lines.length; i++)
+        {
+            lines[i].planif = new require('mongodb').ObjectID(pid);
+            collection.insert(lines[i], function (err, saved) {});
+        }
+    });
+    res.send(true)
 };
