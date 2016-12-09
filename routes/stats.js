335ec7e79829c487b5a3dcb6ba2036ab2d483f5c
fabio <fab.grenouillet@gmail.com>
2016-12-09 11:22:38 +0400

    'stats'

--- routes/stats.js
+++ routes/stats.js
@@ -10,8 +10,9 @@ exports.prevsByDay = function (req, res) {
                 count: { $sum: 1 }
             }}
         ).toArray(function (err, items) {
-                //ret.items = items;
-                res.send(items);
+                var ret;
+                ret.items = items;
+                res.send(ret);
             });
     });  
 };
