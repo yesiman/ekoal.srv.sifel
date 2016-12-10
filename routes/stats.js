cc8153126fb8e44fd7225ef248e09528a493f925
fabio <fab.grenouillet@gmail.com>
2016-12-10 15:48:29 +0400

    stats

--- routes/stats.js
+++ routes/stats.js
@@ -3,7 +3,7 @@ exports.prevsByDay = function (req, res) {
     console.log(req.body.dateFrom);
     console.log(req.body.dateTo);
     db.collection('planifs_lines', function (err, collection) {
-        var obj_ids = req.body.prodsIds.map(function (item){ return ObjectId(item)});
+        var obj_ids = req.body.prodsIds.map(function (item){ return new require('mongodb').ObjectId(item)});
         console.log(obj_ids);
         collection.aggregate(
             { $group : {
