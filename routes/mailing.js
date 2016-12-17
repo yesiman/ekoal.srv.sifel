7d467a9005d3ca00eee6817c77643263e3dcf342
fabio <fab.grenouillet@gmail.com>
2016-12-17 13:30:08 +0400

    forgot password methods

---
+++ routes/mailing.js
@@ -0,0 +1,14 @@
+﻿exports.sendMailRecover = function (req, res) {
+    db.collection('users', function (err, collection) {
+        collection.findOne({ email: req.body.mail }, function (err, item) {
+            if (item) {
+
+                res.send({ success: true });
+            }
+            else {
+                res.send({ success: false });
+            }
+
+        })
+    });
+};
