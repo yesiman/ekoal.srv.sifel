7df4c9feaadf74fee5841aaf21f5e7a67b5d2cc8
fabio <fab.grenouillet@gmail.com>
2016-12-17 13:39:31 +0400

    forgot password methods

--- routes/mailing.js
+++ routes/mailing.js
@@ -2,8 +2,21 @@
     db.collection('users', function (err, collection) {
         collection.findOne({ email: req.body.mail }, function (err, item) {
             if (item) {
+                var api_key = process.env.MAILGUN_API_KEY;
+                var domain = process.env.MAILGUN_DOMAIN;
+                var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain });
 
-                res.send({ success: true });
+                var data = {
+                    from: 'Arifel <no_reply@yesi.test>',
+                    to: req.body.mail,
+                    subject: 'Identifiants',
+                    text: item.email + " " + item.pass
+                };
+
+                mailgun.messages().send(data, function (error, body) {
+                    res.send({ success: true });
+                });
+                
             }
             else {
                 res.send({ success: false });
