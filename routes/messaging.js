a505a323d3d9fdc65f8eee7d0af7faa1288285a2
fabio <fab.grenouillet@gmail.com>
2016-12-27 10:19:13 +0400

    maj parcelles

--- routes/messaging.js
+++ routes/messaging.js
@@ -1,7 +1,7 @@
 var twilio = require('twilio');
 exports.testTwilio = function (req, res) {
     var accountSid = 'AC32c5e4d582dd5c0b87b245f01d436ab1'; // Your Account SID from www.twilio.com/console
-    var authToken = '{{ bfa27f2dc359c626f20f275ec2a63636 }}';   // Your Auth Token from www.twilio.com/console
+    var authToken = 'bfa27f2dc359c626f20f275ec2a63636';   // Your Auth Token from www.twilio.com/console
     var client = new twilio.RestClient(accountSid, authToken);
     client.messages.create({
         body: 'Hello from Node',
