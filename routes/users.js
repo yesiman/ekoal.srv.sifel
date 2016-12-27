403f1fbd1efce23aa1ee71e0ead445d8ecd65c75
fabio <fab.grenouillet@gmail.com>
2016-12-27 09:20:54 +0400

    maj parcelles

--- routes/users.js
+++ routes/users.js
@@ -18,6 +18,8 @@ exports.login = function (req, res) {
 exports.get = function (req, res) {
     db.collection('users', function (err, collection) {
         collection.findOne({ _id: new require('mongodb').ObjectID(req.params.id) }, function (err, item) {
+            console.log("req.params.id",req.params.id);
+            console.log("item",item);
             if (item)
             {
                 if (item.type = 4)//PRODUCTEUR
