d551ceee00a4009cbae81164abe8db7312a645f9
fabio <fab.grenouillet@gmail.com>
2016-12-29 13:59:53 +0400

    vues planinf workflow

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,11 @@ exports.login = function (req, res) {
                 var token = jwt.sign(item, process.env.JWT, {
                     expiresIn: 1440
                 });
-                res.send({success:true, name:item.name, surn:item.surn, type:item.type , token:token}); 
+                res.send({success:true,_id:item._id, name:item.name, surn:item.surn, type:item.type , token:token}); 
             }
             else {
                 res.send({success:false});
-            }
-            
+            }  
         })
     });
 };
