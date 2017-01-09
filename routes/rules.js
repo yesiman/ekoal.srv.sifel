d64d00a7d1069ded530e3789d9eb4e226ec5be11
fabio <fab.grenouillet@gmail.com>
2017-01-09 15:18:44 +0400

    rules methods

---
+++ routes/rules.js
@@ -0,0 +1,52 @@
+exports.get = function (req, res) {
+    db.collection('products_rules', function (err, collection) {
+        collection.findOne({ _id: new require('mongodb').ObjectID(req.params.id) }, function (err, item) {
+            res.send(item);
+        })
+    });
+};
+exports.getAllByProduit = function (req, res) {
+    var ret = new Object();
+    //TODO ADD WORKFLOW RULES
+    db.collection('products_rules', function (err, collection) {
+        collection.count({produit:new require('mongodb').ObjectID(req.params.id)}, function (err, count) {
+            ret.count = count;
+            collection.find().toArray(function (err, items) {
+                ret.items = items;
+                res.send(ret);
+            });
+        });
+    });
+};
+exports.delete = function (req, res) {
+    db.collection('products_rules', function (err, collection) {
+    collection.remove({ _id: new require('mongodb').ObjectID(req.params.id) },
+        function (err, result) {
+            res.send(result);
+        });
+    });
+};
+exports.add = function (req, res) {
+    var pid = req.params.id;
+    req.body.rule.dateModif = new Date();
+    db.collection('products_rules', function (err, collection) {
+        if (pid == "-1")
+        {
+            collection.insert( req.body.rule , function (err, saved) {
+                if (err || !saved) {
+                    res.send(false)
+                }
+                else {
+                    res.send(true);
+                }
+            });
+        }
+        else {
+            delete req.body.rule._id;
+            collection.update(
+                { _id: new require('mongodb').ObjectID(pid) },
+                req.body.rule);
+                res.send(true);
+        }      
+    });
+};
