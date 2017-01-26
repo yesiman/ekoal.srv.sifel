9e69572440c580fa4a343744bb51ea57b0e7cd83
fabio <fab.grenouillet@gmail.com>
2017-01-26 14:37:29 +0400

    maj rules

--- routes/products.js
+++ routes/products.js
@@ -52,13 +52,17 @@ exports.getAllByLib = function (req, res) {
                 console.log(prods);
                 db.collection('products_orgas_specs', function (err, collection) {
                     collection.find({produit: {$in:prods}, user: new require('mongodb').ObjectID(req.decoded._id)}).skip(skip).limit(limit).toArray(function (err, items) {
-                        items.forEach(value)
+                        prods.forEach(value)
                         {
-                            console.log("value",value);
+                            console.log("prods",value);
+                            items.forEach(value)
+                            {
+                                console.log("items",value);
+                            }
                         }
                         //ret.customs = items;
                         //console.log(items);
-                        console.log("ret",ret);
+                        
                         res.send(ret);
                     });
                 });
