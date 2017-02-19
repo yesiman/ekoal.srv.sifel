c94920b45b0c3299843e2923ea619d294535ecf9
fabio <fab.grenouillet@gmail.com>
2017-02-19 20:55:36 +0400

    twilio

--- routes/messaging.js
+++ routes/messaging.js
@@ -6,7 +6,7 @@ exports.testTwilio = function (req, res) {
     var authToken = 'bfa27f2dc359c626f20f275ec2a63636';   // Your Auth Token from www.twilio.com/console
     var client = new twilio.RestClient(accountSid, authToken);
     client.messages.create({
-        body: 'Hello from Node',
+        body: req.body.message.text,
         to: '+262693336223',  // Text this number
         from: '+33644641541' // From a valid Twilio number
     }, function(err, message) {
