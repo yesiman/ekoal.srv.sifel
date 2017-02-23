4cf9037543d2244613f8733c3ae83666fafcc0cc
fabio <fab.grenouillet@gmail.com>
2017-02-23 22:09:12 +0400

    manage groups

--- routes/importer.js
+++ routes/importer.js
@@ -1,7 +1,6 @@
 
 
 exports.produits = function (req, res) {
-    console.log("req.files[0]",req.files[0]);
     var lines = req.files[0].buffer.toString().split("\n");
     var prods = [];
     for (var i = 0; i < lines.length; i++) {
@@ -28,23 +27,26 @@ exports.produits = function (req, res) {
     res.send(true);
 }
 exports.producteurs = function (req, res) {
-    console.log("req.files[0]",req.files[0]);
     var lines = req.files[0].buffer.toString().split("\n");
+    var users = [];
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i].split(";");
+        var user = {
+            type:4,
+            dateModif: new Date(),
+            orga:new require('mongodb').ObjectID(req.decoded.orga),
+            codeAdh:line[2],
+            name:line[4],
+            surn:line[5],
+            pass:line[7],
+            mobPhone:line[8],
+            certif:line[9]
+        };
+        users.push(user);    
+    }
     db.collection('users', function (err, collection) {
-        for (var i = 0; i < lines.length; i++) {
-            var line = lines[i].split(";");
-            var produit = {
-                user:new require('mongodb').ObjectID(req.decoded._id),
-                dateModif: new Date(),
-                orga:new require('mongodb').ObjectID(req.decoded.orga),
-                codeProd:line[0],
-                lib:line[1],
-                rendement:{
-                    val:0,
-                    unit:2
-                }
-            };
-            collection.insert(produit , function (err, saved) { 
+        for (var i = 0; i < users.length; i++) {
+            collection.insert(users[i] , function (err, saved) { 
             });
         }
     });
