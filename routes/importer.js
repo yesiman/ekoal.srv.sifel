eb9b257abff8c583867834f3d7ada1dab46e1f43
fabio <fab.grenouillet@gmail.com>
2017-02-23 20:55:47 +0400

    manage groups

--- routes/importer.js
+++ routes/importer.js
@@ -2,7 +2,7 @@
 
 exports.produits = function (req, res) {
     console.log("req.files",req.files);
-    console.log("req.files.path",req.files[0].path);
+    console.log("req.files.path",req.files[0].buffer.toString());
     
     res.send(true);
 }
