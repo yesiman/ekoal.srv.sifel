4516e0c1cfb4cf9f4185ffe30fc7e991262cf31d
fabio <fab.grenouillet@gmail.com>
2017-02-23 21:28:20 +0400

    manage groups

--- routes/importer.js
+++ routes/importer.js
@@ -2,8 +2,24 @@
 
 exports.produits = function (req, res) {
     var lines = req.files[0].buffer.toString().split("\n");
-    for (var i = 0; i < lines.length; i++) {
-        console.log(lines[i]);
-    }
+    db.collection('products', function (err, collection) {
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i].split(";");
+            var produit = {
+                user:new require('mongodb').ObjectID(req.decoded._id),
+                dateModif: new Date(),
+                orga:new require('mongodb').ObjectID(req.decoded.orga),
+                code:line[0],
+                lib:line[1],
+                rendement:{
+                    val:0,
+                    unit:2
+                }
+            };
+            collection.insert(produit , function (err, saved) {
+                
+            });
+        }
+    });
     res.send(true);
 }
