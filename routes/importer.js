2f9703be886750cd2f69cc6d4a1ac48fc0cdf6a4
fabio <fab.grenouillet@gmail.com>
2017-02-24 20:41:23 +0400

    manage groups

--- routes/importer.js
+++ routes/importer.js
@@ -70,32 +70,56 @@ exports.objectifs = function (req, res) {
     var lines = req.files[0].buffer.toString().split("\n");
     var errors = [];
     var objectifsLines = [];
-    for (var i = 0; i < lines.length; i++) {
-        var lineStr = lines[i];
-        var line = lineStr.split(";");
+    db.collection('products', function (err, collection) {
+        for (var i = 0; i < lines.length; i++) {
+            var lineStr = lines[i];
+            var line = lineStr.split(";");
+            var months = getObjectifMonths();
+
+
+            getObjectifMonthsv2(months,line)
+                .then(
+                // On affiche un message avec la valeur
+                function(val) {
+
+                    console.log("VALLL",val);
+                }).catch(
+                // Promesse rejetée
+                function() { 
+                    console.log("promesse rompue");
+                });
+            
+
+            
+                collection.findOne({ codeProd:{$eq:line[0].toString()},orga:new require('mongodb').ObjectID(req.decoded.orga)}, function (err, item) {
+                    if (item)
+                    {
+                        var objectif = {
+                            produit:new require('mongodb').ObjectID(item._id),
+                            user:new require('mongodb').ObjectID(req.decoded._id),
+                            lines: months
+                        };
+                        db.collection('products_objectifs', function (err, collection) {
+                            collection.update(
+                                { produit: new require('mongodb').ObjectID(objectif.produit) },
+                                objectif, 
+                                { "upsert": true },
+                                function(err, results) {
+                            });
+                        });
+                    }
+                });
+            
+            }
+    });  
+    res.send({success:true});
+    
+    
+}
+
+function getObjectifMonthsv2(months,line){
+  return new Promise(function (fulfill, reject){
         var months = getObjectifMonths();
-
-
-        getObjectifMonthsv2(months,line)
-            .then(
-            // On affiche un message avec la valeur
-            function(val) {
-
-                console.log("VALLL",val);
-            }).catch(
-            // Promesse rejetée
-            function() { 
-                console.log("promesse rompue");
-            });
-
-       /* getObjectifMonthsv2(months,line)
-        .then(result1 => {
-            console.log(result1);
-            return asyncFunction2(x, y);
-        })
-        .then(result2 => {
-            console.log(result2);
-        });*/
         for (var imonth = 0; imonth < months.length; imonth++) {
             months[imonth].rendement = {
                 val:(line[imonth+1].toString().trim()!=""?parseInt(line[imonth+1]):0),
@@ -105,36 +129,8 @@ exports.objectifs = function (req, res) {
                 "1":{val:8}
             };
         }
-
-        db.collection('products', function (err, collection) {
-            collection.findOne({ codeProd:{$eq:line[0].toString()},orga:new require('mongodb').ObjectID(req.decoded.orga)}, function (err, item) {
-                if (item)
-                {
-                    var objectif = {
-                        produit:new require('mongodb').ObjectID(item._id),
-                        user:new require('mongodb').ObjectID(req.decoded._id),
-                        lines: months
-                    };
-                    db.collection('products_objectifs', function (err, collection) {
-                        collection.update(
-                            { produit: new require('mongodb').ObjectID(objectif.produit) },
-                            objectif, 
-                            { "upsert": true },
-                            function(err, results) {
-                        });
-                    });
-                }
-            });
-        });      
-    }
-    res.send({success:true});
-    
-    
-}
-
-function getObjectifMonthsv2(months,line){
-  return new Promise(function (fulfill, reject){
-    fulfill({res:"yesi"});
+        console.log(months);
+        fulfill(months);
   });
 }
 
