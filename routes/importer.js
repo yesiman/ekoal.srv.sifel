d41cd5795cf5b17b2ad7c445f076a76f3a9e95d2
fabio <fab.grenouillet@gmail.com>
2017-02-25 16:37:38 +0400

    importer objectifs

--- routes/importer.js
+++ routes/importer.js
@@ -175,7 +175,7 @@ function getObjectifMonthsv2(line,prd,usr,callback){
             var amountPerW = amount / months[imonth].weeks.length;
             months[imonth].rendements = {};
             console.log("yeah",months[imonth].weeks);
-            for (var iw = 0;iw < months[imonth].weeks;iw++)
+            for (var iw = 0;iw < months[imonth].weeks.length;iw++)
             {
                 months[imonth].rendements[iw] = {val:amountPerW,unit:1};
                 
