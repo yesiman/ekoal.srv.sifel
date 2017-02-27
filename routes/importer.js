9ac31d8e6c4f6cf9e025c6ab6d3aad96d923b240
fabio <fab.grenouillet@gmail.com>
2017-02-27 10:51:19 +0400

    importer objectifs

--- routes/importer.js
+++ routes/importer.js
@@ -52,7 +52,7 @@ exports.producteurs = function (req, res) {
             email:line[6],
             pass:line[7],
             mobPhone:line[8],
-            certif:line[9]
+            certif:line[9].replace("\r","")
         };
         users.push(user);    
     }
