09b34a9dcfc232ef3614145e2dc8fca6e507add7
fabio <fab.grenouillet@gmail.com>
2017-04-07 13:55:43 +0400

    notifiacation producteurs

--- routes/messaging.js
+++ routes/messaging.js
@@ -19,7 +19,7 @@ exports.sendSmsToProducteurs = function(req, res) {
 
 
         // Usage
-        var count = 0;
+        //var count = 0;
         /*promiseWhile(function () { return count <= items.length; }, function () {
             var smsDatas = {};
             var pla = items[count];
@@ -47,75 +47,55 @@ exports.sendSmsToProducteurs = function(req, res) {
                 console.log("smsDatas",smsDatas);
                 count++;
             });
-        });
+        });*/
 
             for (var i = 0;i < items.length;i++)
             {
                 //GROUPER TEXTE PAR PRODUCTEUR
-                var smsDatas = {};
-                var pla = items[i];
-                //GET PLANIF_LINE
-                getPlanifLine(pla.planif).then(function (data) {
-                    smsDatas.pl = data;
-                    //GET PRODUCTEUR
-                    getUser(smsDatas.pl.producteur).then(function (data) {
-                        smsDatas.u = data;
-                        //GET PRODUIT
-                         getProduit(smsDatas.pl.produit).then(function (data) {
-                             smsDatas.p = data;
-                            console.log("smsDatas",smsDatas);
-
-                            
-
-                            //SEND
-                            sendSms(smsDatas).then(function (data) {
-                                console.log(data);
-                                //UPDATE WITH TWILIO ID
-                                var pid = pla._id;
-                                delete pla._id;
-                                pla.sent = true;
-                                pla.dateSent = new Date();
-                                pla.sid = data.sid;
-                                collection.update(
-                                    { _id: new require('mongodb').ObjectID(pid) },
-                                    pla);
-                            });
-                        });
-                    })
-                })
-                .catch(console.error);
-            }*/
+                makeSmsSend(items[i]);
+            }
         });
     });
     //res.send("ok");
 }
 
-var Q = require("q");
+function makeSmsSend (pla) {
+    return new Promise(function (resolve, reject) {
+      var smsDatas = {};
+    //GET PLANIF_LINE
+    getPlanifLine(pla.planif).then(function (data) {
+        smsDatas.pl = data;
+        //GET PRODUCTEUR
+        getUser(smsDatas.pl.producteur).then(function (data) {
+            smsDatas.u = data;
+            //GET PRODUIT
+                getProduit(smsDatas.pl.produit).then(function (data) {
+                    smsDatas.p = data;
+                console.log("smsDatas",smsDatas);
 
-// `condition` is a function that returns a boolean
-// `body` is a function that returns a promise
-// returns a promise for the completion of the loop
-function promiseWhile(condition, body) {
-    var done = Q.defer();
+                
 
-    function loop() {
-        // When the result of calling `condition` is no longer true, we are
-        // done.
-        if (!condition()) return done.resolve();
-        // Use `when`, in case `body` does not return a promise.
-        // When it completes loop again otherwise, if it fails, reject the
-        // done promise
-        Q.when(body(), loop, done.reject);
-    }
+                //SEND
+                /*sendSms(smsDatas).then(function (data) {
+                    console.log(data);
+                    //UPDATE WITH TWILIO ID
+                    var pid = pla._id;
+                    delete pla._id;
+                    pla.sent = true;
+                    pla.dateSent = new Date();
+                    pla.sid = data.sid;
+                    collection.update(
+                        { _id: new require('mongodb').ObjectID(pid) },
+                        pla);
+                });*/
+                resolve({ok:"ok"}); 
+            });
+        })
+    })
+    .catch(console.error);
 
-    // Start running the loop in the next tick so that this function is
-    // completely async. It would be unexpected if `body` was called
-    // synchronously the first time.
-    Q.nextTick(loop);
-
-    // The promise
-    return done.promise;
-}
+  })
+} 
 
 
 
