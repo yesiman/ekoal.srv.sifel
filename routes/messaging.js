0e738f34efb52a3629bd4e988b80865ffba15b0e
fabio <fab.grenouillet@gmail.com>
2017-04-07 12:56:20 +0400

    notifiacation producteurs

--- routes/messaging.js
+++ routes/messaging.js
@@ -68,6 +68,44 @@ exports.sendSmsToProducteurs = function(req, res) {
     //res.send("ok");
 }
 
+var Q = require("q");
+
+// `condition` is a function that returns a boolean
+// `body` is a function that returns a promise
+// returns a promise for the completion of the loop
+function promiseWhile(condition, body) {
+    var done = Q.defer();
+
+    function loop() {
+        // When the result of calling `condition` is no longer true, we are
+        // done.
+        if (!condition()) return done.resolve();
+        // Use `when`, in case `body` does not return a promise.
+        // When it completes loop again otherwise, if it fails, reject the
+        // done promise
+        Q.when(body(), loop, done.reject);
+    }
+
+    // Start running the loop in the next tick so that this function is
+    // completely async. It would be unexpected if `body` was called
+    // synchronously the first time.
+    Q.nextTick(loop);
+
+    // The promise
+    return done.promise;
+}
+
+
+// Usage
+var index = 1;
+promiseWhile(function () { return index <= 11; }, function () {
+    console.log(index);
+    index++;
+    return Q.delay(500); // arbitrary async
+}).then(function () {
+    console.log("done");
+}).done();
+
 function getPlanifLine (id) {
   return new Promise(function (resolve, reject) {
       db.collection('planifs_lines', function (err, collection) {
