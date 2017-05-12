ea228719ca3723f0751aa85e20a8a5c1765516c6
fabio <fab.grenouillet@gmail.com>
2017-05-12 09:30:18 +0400

    moment integration for correct time

--- routes/_shared.js
+++ routes/_shared.js
@@ -1,7 +1,7 @@
 var moment = require('moment-timezone');
 function getReunionLocalDate()
 {
-    return new Date(moment.tz(new Date().getTime(),"Indian/Reunion").format("MMM DD, YYYY HH:mm:SS"));
+    return new Date(moment.tz(new Date().getTime(),"Indian/Reunion").format("MMM DD, YYYY HH:mm:ss:SSS"));
 };
 
 module.exports = {
