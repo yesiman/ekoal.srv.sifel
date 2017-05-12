2630443f58df1ce316f593cf5a0932282d853ef6
fabio <fab.grenouillet@gmail.com>
2017-05-12 08:52:26 +0400

    moment integration for correct time

--- server.js
+++ server.js
@@ -203,5 +203,5 @@ Date.prototype.getWeek = function() {
 };
 function getReunionLocalDate()
 {
-    return moment.tz(new Date().getTime(),"Indian/Reunion").format();
+    return new Date(moment.tz(new Date().getTime(),"Indian/Reunion").format());
 };
