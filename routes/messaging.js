2d2dc4c13b1c18fa10a53721197cffd5d2a27419
fabio <fab.grenouillet@gmail.com>
2017-05-23 21:00:55 +0400

    instag test

--- routes/messaging.js
+++ routes/messaging.js
@@ -179,8 +179,8 @@ exports.smsReceive = function(req, res)
 
 exports.instagReceive = function(req, res)
 {
-    console.log("lmlml");
+    //console.log("lmlml");
     //console.log("req.query.hub.challenge",req.query.hub.challenge);
     console.log("req.query",req.query);
-    res.send(req.query.hub.challenge);
+    res.send(req.query["hub.challenge"]);
 }
