712c8a22418d6b979a105f3d51e0184c328f4cbb
fabio <fab.grenouillet@gmail.com>
2017-06-28 14:12:35 +0400

    maj importer mobile

--- routes/from_mobile.js
+++ routes/from_mobile.js
@@ -1,39 +1,38 @@
 
 exports.uploadDatas = function (req, res) {
     var lines = req.body.lines;
-    db.collection('parcelles', function (err, collection) {
     for (var i = 0; i < lines.length; i++) {
         switch(lines[i].type)
         {
             case "parcelle":
                 //SET MONGOGEO CORRECTLY "POLYGON"
-                collection.findOne({ _id: new require('mongodb').ObjectID(lines[i]._id) }, function (err, item) {
-                    if (item)
-                    {
-                        updParcelle(item._id,lines[i].surface,lines[i].altitude,{ type: "Polygon", coordinates: lines[i].coordonnees})
-                    }
-                });
+                updParcelle(lines[i]._id,lines[i].surface,lines[i].altitude,{ type: "Polygon", coordinates: lines[i].coordonnees})
                 break;
         }
     }
 
     res.send({success:true});
-    });
+    
 }
 
 function updParcelle(id,surface,altitude,coordonnees) {
   return new Promise(function (resolve, reject) {
       db.collection('parcelles', function (err, collection) {
-        collection.update(
-            { _id: new require('mongodb').ObjectID(id) },
+          collection.findOne({ _id: new require('mongodb').ObjectID(id) }, function (err, item) {
+            if (item)
             {
-                $set:{
-                    surface:surface,
-                    altitude:altitude,
-                    coordonnees:coordonnees
-                }
-            }, 
-            { "upsert": true });
+                collection.update(
+                    { _id: new require('mongodb').ObjectID(id) },
+                    {
+                        $set:{
+                            surface:surface,
+                            altitude:altitude,
+                            coordonnees:coordonnees
+                        }
+                    }, 
+                    { "upsert": true });
+            }
         });
+    });
   })
 }
