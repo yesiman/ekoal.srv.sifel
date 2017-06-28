496a9a218eba4094402825b76a2cc4d3856e0e51
fabio <fab.grenouillet@gmail.com>
2017-06-28 14:04:08 +0400

    maj importer mobile

--- routes/from_mobile.js
+++ routes/from_mobile.js
@@ -5,28 +5,32 @@ exports.uploadDatas = function (req, res) {
         switch(lines[i].type)
         {
             case "parcelle":
-                db.collection('parcelles', function (err, collection) {
-                    //SET MONGOGEO CORRECTLY "POLYGON"
-                   collection.findOne({ _id: new require('mongodb').ObjectID(lines[i]._id) }, function (err, item) {
-                       if (item)
-                       {
-                           console.log("nualt",lines[i].altitude);
-                           collection.update(
-                            { _id: new require('mongodb').ObjectID(item._id) },
-                            {
-                                $set:{
-                                    surface:lines[i].surface,
-                                    altitude:lines[i].altitude,
-                                    coordonnees:{ type: "Polygon", coordinates: lines[i].coordonnees}
-                                }
-                            }, 
-                            { "upsert": true });
-                       }
-                   });
-                    
+                //SET MONGOGEO CORRECTLY "POLYGON"
+                collection.findOne({ _id: new require('mongodb').ObjectID(lines[i]._id) }, function (err, item) {
+                    if (item)
+                    {
+                        updParcelle(item._id,lines[i].surface,lines[i].altitude,{ type: "Polygon", coordinates: lines[i].coordonnees})
+                    }
                 });
                 break;
         }
     }
     res.send({success:true});
+}
+
+function updParcelle(id,surface,altitude,coordonnees) {
+  return new Promise(function (resolve, reject) {
+      db.collection('parcelles', function (err, collection) {
+        collection.update(
+            { _id: new require('mongodb').ObjectID(id) },
+            {
+                $set:{
+                    surface:surface,
+                    altitude:altitude,
+                    coordonnees:coordonnees
+                }
+            }, 
+            { "upsert": true });
+        });
+  })
 }
