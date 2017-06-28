080226b170ecff426b66816a72f597e8c0bf9358
fabio <fab.grenouillet@gmail.com>
2017-06-28 11:50:34 +0400

    maj importer mobile

--- routes/from_mobile.js
+++ routes/from_mobile.js
@@ -3,7 +3,7 @@ exports.uploadDatas = function (req, res) {
     var lines = req.body.lines;
     db.collection('parcelles', function (err, collection) {
         for (var i = 0; i < lines.length; i++) {
-            console.log(line,lines[i]);
+            console.log("line",lines[i]);
             //SET MONGOGEO CORRECTLY "POLYGON"
             /*collection.update(
                     { _id: lines[i]._id },
