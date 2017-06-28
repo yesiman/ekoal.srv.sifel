9be0c0e2ea42dd3414b94c5eb613ab2cf51f31cd
fabio <fab.grenouillet@gmail.com>
2017-06-28 12:00:49 +0400

    maj importer mobile

--- routes/from_mobile.js
+++ routes/from_mobile.js
@@ -1,31 +1,28 @@
 
 exports.uploadDatas = function (req, res) {
     var lines = req.body.lines;
-    
-        for (var i = 0; i < lines.length; i++) {
-            switch(lines[i].type)
-            {
-                case "parcelle":
-                    db.collection('parcelles', function (err, collection) {
-                        //SET MONGOGEO CORRECTLY "POLYGON"
-                        collection.update(
-                            { _id: new require('mongodb').ObjectID(lines[i]._id) },
-                            {
-                                $set:{
-                                    surface:lines[i].surface,
-                                    altitude:lines[i].altitude,
-                                    coordonnees:{ type: "Polygon", coordinates: lines[i].coordonnees}
-                                }
-                            }, 
-                            { "upsert": true },
-                            function(err, results) {
-                            });
+    for (var i = 0; i < lines.length; i++) {
+        switch(lines[i].type)
+        {
+            case "parcelle":
+                db.collection('parcelles', function (err, collection) {
+                    //SET MONGOGEO CORRECTLY "POLYGON"
+                    collection.update(
+                        { _id: new require('mongodb').ObjectID(lines[i]._id) },
+                        {
+                            $set:{
+                                surface:lines[i].surface,
+                                altitude:lines[i].altitude,
+                                coordonnees:{ type: "Polygon", coordinates: lines[i].coordonnees}
+                            }
+                        }, 
+                        { "upsert": true },
+                        function(err, results) {
+
                         });
-                    }
-                    break;
-            }
-            
+                });
+                break;
         }
-        res.send({success:true});
-    
+    }
+    res.send({success:true});
 }
