6b653eba85d7b4565b273becfb0d604ec0fe8657
fabio <fab.grenouillet@gmail.com>
2017-10-11 13:56:50 +0200

    up sort bons

--- routes/bons.js
+++ routes/bons.js
@@ -804,16 +804,22 @@ exports.getStatProducteursExp = function (req, res) {
                             for(var i=0;i<items.length;i++)
                             {
                                 ret += items[i].name + " " + items[i].surn + ";";
-                                var sum = 0;
+                                var sumLoc = 0;
+                                var sumExp = 0;
                                 for(var i2=0;i2<datas.length;i2++)
                                 {
                                     if(datas[i2]._id.producteur.toString() == items[i]._id.toString())
                                     {
-                                       sum += datas[i2].count;
-                                    }
-                                    
+                                       if(datas[i2]._id.destination == "local")
+                                        {
+                                            sumLoc += datas[i2].count;
+                                        }
+                                        else {
+                                            sumExp += datas[i2].count;
+                                        }
+                                    }   
                                 }
-                                ret +=  sum.toString() + "\n";
+                                ret += sumLoc + ";" + sumExp + "\n";
                             }
 
                             res.set('Content-Type', 'application/octet-stream');
@@ -872,16 +878,22 @@ exports.getStatClientsExp = function (req, res) {
                             for(var i=0;i<items.length;i++)
                             {
                                 ret += items[i].name + " " + items[i].surn + ";";
-                                var sum = 0;
+                                var sumLoc = 0;
+                                var sumExp = 0;
                                 for(var i2=0;i2<datas.length;i2++)
                                 {
                                     if(datas[i2]._id.client.toString() == items[i]._id.toString())
                                     {
-                                       sum += datas[i2].count;
-                                    }
-                                    
+                                       if(datas[i2]._id.destination == "local")
+                                        {
+                                            sumLoc += datas[i2].count;
+                                        }
+                                        else {
+                                            sumExp += datas[i2].count;
+                                        }
+                                    }   
                                 }
-                                ret +=  sum.toString() + "\n";
+                                ret += sumLoc + ";" + sumExp + "\n";
                             }
 
                             res.set('Content-Type', 'application/octet-stream');
@@ -940,15 +952,22 @@ exports.getStatStationsExp = function (req, res) {
                             for(var i=0;i<items.length;i++)
                             {
                                 ret += items[i].lib + ";";
-                                var sum = 0;
+                                var sumLoc = 0;
+                                var sumExp = 0;
                                 for(var i2=0;i2<datas.length;i2++)
                                 {
                                     if(datas[i2]._id.station.toString() == items[i]._id.toString())
                                     {
-                                       sum += datas[i2].count;
+                                       if(datas[i2]._id.destination == "local")
+                                        {
+                                            sumLoc += datas[i2].count;
+                                        }
+                                        else {
+                                            sumExp += datas[i2].count;
+                                        }
                                     }   
                                 }
-                                ret +=  sum + "\n";
+                                ret += sumLoc + ";" + sumExp + "\n";
                             }
 
                             res.set('Content-Type', 'application/octet-stream');
